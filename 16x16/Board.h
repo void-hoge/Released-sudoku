65287ca5b297f49819b9c2bb9ee641bbfaabc1d1
void-hoge <coilgunrailgun@gmail.com>
2017-06-30 23:04:28 +0900

    add 16x16 and 9x9

---
+++ 16x16/Board.h
@@ -0,0 +1,29 @@
+#ifndef __BOARD_H_INCLUDED__
+#define __BOARD_H_INCLUDED__
+
+#include <bitset>
+using namespace std;
+
+#define base bitset<256>
+
+class Board{
+public:
+	Board();
+	void input();
+	void output();
+	int check(int);
+	void put(int, int);
+	bool update();
+	bitset<16> setableNumber(int);
+	int emptyCell();
+	bool isFinish();
+private:
+	base cells[16];
+	base confirmedCells[16];
+	void set(int, int);
+	void updateConfirmedCells();
+	void setConfirmedCells(int, int);
+	bool checkError();
+};
+
+#endif
